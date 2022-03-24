6b98cad9904119ccbda44fe54dc58b78ebc8e030
Jaden Willeiksen <perryjaden@yahoo.com>
2022-03-24 15:15:21 -0700

    new algos

--- JavaAlgo.java
+++ JavaAlgo.java
@@ -19,6 +19,11 @@ public class JavaAlgo {
         // SquareDigit squareDigit = new SquareDigit();
         // squareDigit.squareDigits(12345);
 
+        // -------------------------------------------------------------------------------------------
+        // Return the number (count) of vowels in the given string.
+        // We will consider a, e, i, o, u as vowels but not y.
+        // The input string will only consist of lower case letters and/or spaces.
+
         class Vowels {
             public int getCount(String str) {
                 int vowelsCount = 0;
