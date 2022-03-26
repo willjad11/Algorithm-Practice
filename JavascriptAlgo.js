6205db73ca3b48004fda2259e956640438f96034
Jaden Willeiksen <perryjaden@yahoo.com>
2022-03-25 22:59:28 -0700

    new algos

--- JavascriptAlgo.js
+++ JavascriptAlgo.js
@@ -113,7 +113,7 @@ function findNextSquare(sq) {
 
 //--------------------------------------------------------------------------------------------
 
-// Take 2 strings s1 and s2 including only letters from ato z.Return a new sorted string, the longest 
+// Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest 
 // possible, containing distinct letters - each taken only once - coming from s1 or s2.
 
 function longest(s1, s2) {
