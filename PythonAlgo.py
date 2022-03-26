f2ab9a04912270847ab7f04ac531a4c330f2f222
Jaden Willeiksen <perryjaden@yahoo.com>
2022-03-25 23:08:50 -0700

    new algos

--- PythonAlgo.py
+++ PythonAlgo.py
@@ -73,7 +73,6 @@ def DNA_strand(dna):
 
 # ----------------------------------------------------------------------
 
-
 # Create a function that returns the sum of the two lowest positive numbers 
 # given an array of minimum 4 positive integers. No floats or non-positive 
 # integers will be passed.
