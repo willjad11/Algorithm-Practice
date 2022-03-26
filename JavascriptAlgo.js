83cc8ab98c5dd4b864b349b279969d19419f1efa
Jaden Willeiksen <perryjaden@yahoo.com>
2022-03-26 15:54:21 -0700

    more algos

--- JavascriptAlgo.js
+++ JavascriptAlgo.js
@@ -141,3 +141,34 @@ function getMiddle(s) {
     return s[(s.length - 1) / 2];
 }
 // console.log(getMiddle("test"))
+
+//--------------------------------------------------------------------------------------------
+
+// Given a string of words, you need to find the highest scoring word. Each letter of a word scores 
+// points according to its position in the alphabet: a = 1, b = 2, c = 3 etc. You need to return the 
+// highest scoring word as a string. If two words score the same, return the word that appears earliest 
+// in the original string. All letters will be lowercase and all inputs will be valid.
+
+function high(x) {
+    const letterDict = {
+        'a': 1, 'b': 2, 'c': 3,
+        'd': 4, 'e': 5, 'f': 6,
+        'g': 7, 'h': 8, 'i': 9,
+        'j': 10, 'k': 11, 'l': 12,
+        'm': 13, 'n': 14, 'o': 15,
+        'p': 16, 'q': 17, 'r': 18,
+        's': 19, 't': 20, 'u': 21,
+        'v': 22, 'w': 23, 'x': 24,
+        'y': 25, 'z': 26};
+    let bestWord = { 'default': 0 };
+    for (word of x.split(' ')) {
+        let wordScore = 0;
+        word.split('').forEach(l => wordScore += letterDict[l]);
+        if (wordScore > Object.values(bestWord)[0]) {
+            delete bestWord[Object.keys(bestWord)[0]];
+            bestWord[word] = wordScore;
+        }
+    }
+    return Object.keys(bestWord)[0];
+}
+// console.log(high('man i need a taxi up to ubud'))
--- PythonAlgo.py
+++ PythonAlgo.py
@@ -116,3 +116,14 @@ def solution(s):
             output += s[len(output) - output.count(" "):i + 1]
     return output
 # print(solution("helloWorld"))
+
+# ----------------------------------------------------------------------
+
+# A Narcissistic Number is a positive number which is the sum of its own digits, 
+# each raised to the power of the number of digits in a given base. Your code must 
+# return true or false(not 'true' and 'false') depending upon whether the given 
+# number is a Narcissistic number in base 10.
+
+def narcissistic(value):
+    return sum(int(x) ** len(str(value)) for x in str(value)) == value
+# print(narcissistic(371))
