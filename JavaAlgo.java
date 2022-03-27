136f83812cb45bd1c1778adbb65c9121aacda081
Jaden Willeiksen <perryjaden@yahoo.com>
2022-03-27 10:18:48 -0700

    new algos

--- JavaAlgo.java
+++ JavaAlgo.java
@@ -253,6 +253,11 @@ public class JavaAlgo {
         // DuplicateEncoder duplicateEncoder = new DuplicateEncoder();
         // System.out.println(duplicateEncoder.encode("Prespecialized"));
 
+        // -------------------------------------------------------------------------------------------
+
+        // Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing 
+        // in this way until a single-digit number is produced. The input will be a non-negative integer.
+
         class DRoot {
             public int digital_root(int n) {
                 String strNum = "" + n;
